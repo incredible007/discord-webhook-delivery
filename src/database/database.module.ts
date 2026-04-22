01c62094010957014b92c9d53da0269544305aaf
Andrew P. <me@andrewpodgola.pro>
2026-04-22 18:54:03 +0300

    CQ-15 Setup Drizzle ORM

---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Global, Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import * as postgres from 'postgres'
+
+import * as schema from './schema'
+
+export const DB_DRIZZLE = Symbol.for('DB_DRIZZLE')
+
+@Global()
+@Module({
+    providers: [
+        {
+            provide: DB_DRIZZLE,
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService): PostgresJsDatabase<typeof schema> => {
+                const url = configService.get<string>('DB_URL')!
+                const client = postgres(url)
+                return drizzle(client, { schema })
+            },
+        },
+    ],
+    exports: [DB_DRIZZLE],
+})
+export class DatabaseModule {}
