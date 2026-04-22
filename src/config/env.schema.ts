4826fd0a941417b9252a2ed0f624b901f95dcec6
Andrew P. <me@andrewpodgola.pro>
2026-04-22 19:00:22 +0300

    CQ-23 Add environment configuration

---
+++ src/config/env.schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+    PORT: z.coerce.number().default(3000),
+    NODE_ENV: z.enum(['DEV', 'PROD', 'TEST']).default('DEV'),
+
+    APP_URL: z.string().url(),
+    DB_URL: z.string().url(),
+})
+
+export type Env = z.infer<typeof envSchema>
