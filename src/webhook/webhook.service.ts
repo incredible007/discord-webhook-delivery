544a3f29d240763379183ca070a489cc7f6bdcf5
Andrew P. <me@andrewpodgola.pro>
2026-04-23 20:22:32 +0300

    delete unused import in service

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,10 +1,6 @@
-import { InjectQueue } from '@nestjs/bullmq'
 import { ConflictException, Inject, Injectable } from '@nestjs/common'
-import { Queue } from 'bullmq'
 
-import { WEBHOOK_JOB, WEBHOOK_QUEUE } from '@/common/constants'
 import { EventVariants } from '@/database/types'
-import { WebhookJobPayloadI } from '@/webhook/interfaces/webhook-job-payload.interface'
 import { WebhookPayloadI } from '@/webhook/interfaces/webhook-payload.interface'
 import {
     WEBHOOK_REPOSITORY,
@@ -16,8 +12,6 @@ export class WebhookService {
     constructor(
         @Inject(WEBHOOK_REPOSITORY)
         private readonly webhookRepository: WebhookRepositoryI,
-        @InjectQueue(WEBHOOK_QUEUE)
-        private readonly webhookQueue: Queue<WebhookJobPayloadI>,
     ) {}
 
     async enqueue(variant: EventVariants, payload: WebhookPayloadI): Promise<void> {
